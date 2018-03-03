50a7eb48ebbf28dc72b5bc526e4df04f50b7a69d
raghukul01 <raghukul.raman01@gmail.com>
2018-03-03 19:06:38 +0530

    step 13

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
+void micrkernel_sendmsg(char *);
+
 void main()	{
 	printf("Helloworld!\n");
+    micrkernel_sendmsg("is more portable");
+}
+
+void micrkernel_sendmsg(char *a) {
+    printf("microkernel: %s\n", a);
 }
